31fc1c32ed8516bef4e17c6676d7b50e6000a971
John <John@0c92f239-eb13-0410-ac4f-b6215be077db>
2007-08-07 11:56:13 +0000

    Removed file/folder
    git-svn-id: http://svn.dsource.org/projects/juno/trunk@139 0c92f239-eb13-0410-ac4f-b6215be077db

--- juno/io/all.d
+++
@@ -1,6 +0,0 @@
-module juno.io.all;
-
-public import juno.io.core,
-  juno.io.path,
-  juno.io.file,
-  juno.io.directory;
--- juno/xml/all.d
+++
@@ -1,6 +0,0 @@
-module juno.xml.all;
-
-public import juno.xml.constants,
-  juno.xml.core,
-  juno.xml.dom,
-  juno.xml.xsl;
