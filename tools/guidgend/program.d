4f8626ccac9e847f4e8bf9cfeffdffb2622f7999
Richard Webb <webby@beardmouse.org.uk>
2011-11-11 16:08:00 +0000

    Update guidgend to build in DMD2

--- tools/guidgend/program.d
+++ tools/guidgend/program.d
@@ -1,7 +1,7 @@
 module guidgen.program;
 
 import juno.com.core;
-import std.stdio : writefln;
+import std.stdio : writefln, writeln;
 
 void main() {
   GUID guid = GUID.create();
@@ -9,9 +9,9 @@ void main() {
   string s = guid.toString("D");
   writefln("{ 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s, 0x%s }", 
               s[0 .. 8], s[9 .. 13], s[14 .. 18], s[19 .. 21], s[21 .. 23], s[24 .. 26], s[26 .. 28], s[28 .. 30], s[30 .. 32], s[32 .. 34], s[34 .. 36]);
-  writefln();
+  writeln();
 
   string ps = guid.toString("P");
   writefln(ps);
-  writefln();
+  writeln();
 }
